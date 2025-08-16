9d41b71597a1ef927e233fdd35bf41982dbb8bb8
shchepylooksana-ux <shchepylooksana@gmail.com>
2025-08-16 23:37:44 +0300

    Initial commit

---
+++ app/src/main/java/com/example/visatracker/MainActivity.kt
@@ -0,0 +1,22 @@
+
+package com.example.visatracker
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            VisaTrackerApp()
+        }
+    }
+}
+
+@Composable
+fun VisaTrackerApp() {
+    Text("Hello, Visa Tracker!")
+}
